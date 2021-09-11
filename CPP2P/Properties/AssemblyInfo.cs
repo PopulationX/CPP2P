9381c6dfcf18b6ed03b2e85d22367d0654763486
Dave <Dave@DESKTOP-2G4NNB6>
2021-09-11 14:27:57 +0100

    Add project files.

---
+++ CPP2P/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CPP2P")]
+[assembly: AssemblyDescription("Cheese Pizza P2P software over TOR")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cheese Pizza")]
+[assembly: AssemblyProduct("CPP2P")]
+[assembly: AssemblyCopyright("Copyright © Cheese Pizza 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3386ad95-fa31-4460-bdc8-7334791c91ec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
